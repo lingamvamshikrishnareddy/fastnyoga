38e4bbee8692d5cd2625fa5651a20178cd0f8428
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2025-06-15 15:58:49 +0530

    Delete backend/generate-jwt-secret.js

--- backend/generate-jwt-secret.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function generateJWTSecret(length = 64) {
-  return crypto.randomBytes(length).toString('hex');
-}
-
-const jwtSecret = generateJWTSecret();
-console.log('Your JWT Secret:');
-console.log(jwtSecret);
